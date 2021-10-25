03ff90e5f3c0ca80f3da667d4d39e06eb6b17dc5
Brent Swisher <swisherb@gvsu.edu>
2021-10-25 10:25:51 -0400

    Add SelectInput story

--- src/PublicationEditor/components/Inputs/SelectInput/stories.js
+++ src/PublicationEditor/components/Inputs/SelectInput/stories.js
@@ -1,31 +1,42 @@
 import { useState } from 'react';
-// import { text, number, boolean } from '@storybook/addon-knobs';
 import SelectInput from './';
 
-export default { title: 'Inputs/SelectInput', component: SelectInput };
+export default {
+	title: 'Inputs/SelectInput',
+	component: SelectInput,
+	args: {
+		name: 'alignment',
+		label: 'Alignment:',
+		labelHidden: false,
+		helpText: '',
+		options: [
+			{
+				value: 'left',
+				title: 'Left',
+			},
+			{
+				value: 'right',
+				title: 'Right',
+			},
+			{
+				value: 'center',
+				title: 'Centered',
+			},
+		],
+	},
+};
 
 export const _default = ( args ) => {
 	const [ value, setValue ] = useState( args.value ?? '' );
 	return (
 		<SelectInput
-			name="alignment"
-			label="Alignment:"
+			name={ args.name }
+			label={ args.label }
+			labelHidden={ args.labelHidden }
+			helpText={ args.helpText }
 			onChange={ setValue }
 			value={ value }
-			options={ [
-				{
-					value: 'left',
-					title: 'Left',
-				},
-				{
-					value: 'right',
-					title: 'Right',
-				},
-				{
-					value: 'center',
-					title: 'Centered',
-				},
-			] }
+			options={ args.options }
 		/>
 	);
 };
