e1ef7b3f5460f7a866c985244cd0c82bd2a9ef4d
Brent Swisher <swisherb@gvsu.edu>
2021-11-11 11:54:18 -0500

    Add show/hide row buttons on mouse enter/leave

--- src/PublicationEditor/components/ControlBlock/index.js
+++ src/PublicationEditor/components/ControlBlock/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Draggable } from 'react-beautiful-dnd';
 
 export function ControlBlock( {
@@ -12,6 +12,7 @@ export function ControlBlock( {
 	disableDelete,
 	children,
 } ) {
+	const [ isFocused, setFocused ] = useState( false );
 	return (
 		<Draggable draggableId={ id } index={ index }>
 			{ ( provided ) => (
@@ -20,35 +21,41 @@ export function ControlBlock( {
 					key={ id }
 					{ ...provided.draggableProps }
 					ref={ provided.innerRef }
+					onMouseEnter={ () => setFocused( true ) }
+					onMouseLeave={ () => setFocused( false ) }
 				>
 					{ children }
-					<span
-						className="block-button-drag"
-						{ ...provided.dragHandleProps }
-					>
-						Drag
-					</span>
-					<button
-						className="block-button-up"
-						disabled={ disableUp }
-						onClick={ onMoveUp }
-					>
-						Up
-					</button>
-					<button
-						className="block-button-down"
-						disabled={ disableDown }
-						onClick={ onMoveDown }
-					>
-						Down
-					</button>
-					<button
-						className="block-button-delete"
-						disabled={ disableDelete }
-						onClick={ onDelete }
-					>
-						Delete
-					</button>
+					{ isFocused && (
+						<React.Fragment>
+							<span
+								className="block-button-drag"
+								{ ...provided.dragHandleProps }
+							>
+								Drag
+							</span>
+							<button
+								className="block-button-up"
+								disabled={ disableUp }
+								onClick={ onMoveUp }
+							>
+								Up
+							</button>
+							<button
+								className="block-button-down"
+								disabled={ disableDown }
+								onClick={ onMoveDown }
+							>
+								Down
+							</button>
+							<button
+								className="block-button-delete"
+								disabled={ disableDelete }
+								onClick={ onDelete }
+							>
+								Delete
+							</button>
+						</React.Fragment>
+					) }
 				</div>
 			) }
 		</Draggable>
