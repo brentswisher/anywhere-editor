f916303ea71ca77aeb4efef3d5b7b90b00559f85
Brent Swisher <swisherb@gvsu.edu>
2021-12-01 10:25:55 -0500

    Remove old testing data

--- src/App.js
+++ src/App.js
@@ -1,57 +1,6 @@
 import PublicationEditor from './PublicationEditor';
 
 function App() {
-	// const testData = [
-	// 	{
-	// 		id: '11111',
-	// 		type: 'article-content',
-	// 		innerContent: [
-	// 			{
-	// 				id: '3766',
-	// 				type: 'Heading',
-	// 				data: { text: 'Apple', level: 1 },
-	// 			}, //Block level data, array to eventually allow rows
-	// 		],
-	// 	},
-	// 	{
-	// 		id: '22222',
-	// 		type: 'article-content',
-	// 		innerContent: [
-	// 			{
-	// 				id: '4891',
-	// 				type: 'Heading',
-	// 				data: { text: 'Kiwi', level: 1 },
-	// 			},
-	// 			{
-	// 				id: '48931',
-	// 				type: 'Heading',
-	// 				data: { text: 'Kiwi Berry', level: 1 },
-	// 			},
-	// 		],
-	// 	},
-	// 	{
-	// 		id: '33333',
-	// 		type: 'article-content',
-	// 		innerContent: [
-	// 			{
-	// 				id: '5638',
-	// 				type: 'Heading',
-	// 				data: { text: 'Banana', level: 1 },
-	// 			},
-	// 		],
-	// 	},
-	// 	{
-	// 		id: '44444',
-	// 		type: 'article-content',
-	// 		innerContent: [
-	// 			{
-	// 				id: '433',
-	// 				type: 'Heading',
-	// 				data: { text: 'Grape', level: 1 },
-	// 			},
-	// 		],
-	// 	},
-	// ];
 	const testData = [
 		{
 			id: '11111',
