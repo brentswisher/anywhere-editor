b1bdd07636d57caf029a339c795573f918cae759
Brent Swisher <swisherb@gvsu.edu>
2022-02-09 14:25:10 -0500

    Set up initial content

--- src/index.js
+++ src/index.js
@@ -11,8 +11,44 @@ export function AnywhereEditor( {
 	config,
 	customControls,
 } ) {
-	const initalContent =
-		! content.length && template.length ? template : content;
+	let initalContent;
+	if ( content.length ) {
+		initalContent = content;
+	} else if ( template.length ) {
+		initalContent = template;
+	} else {
+		//Default to a blank content control so it isn't empty which is confusing to users
+		initalContent = [
+			{
+				id: '5249',
+				type: 'article-content',
+				position: 'position-offset-full',
+				innerContent: [
+					{
+						id: '8466',
+						type: 'Heading',
+						data: { text: 'Hello!', level: 1 },
+					},
+				],
+			},
+			{
+				id: '9544',
+				type: 'article-content',
+				position: 'position-full',
+				innerContent: [
+					{
+						id: '89',
+						type: 'Content',
+						data: {
+							title: '',
+							content:
+								"<p>This is where you can edit your article's content. Use the buttons on the bottom to add different kinds of content. You can move a row and adjust its position on the page by using the buttons on the upper left of a block when you hover over it.</p>",
+						},
+					},
+				],
+			},
+		];
+	}
 	return (
 		<Provider store={ store }>
 			<div id="anywhere-editor">
