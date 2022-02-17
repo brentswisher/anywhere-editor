f9250c73506f8a180f97221bbb24f112808ee703
Brent Swisher <swisherb@gvsu.edu>
2022-02-17 16:31:33 -0500

    Release 0.1.8

--- dist/components/Inputs/RichTextInput/index.js
+++ dist/components/Inputs/RichTextInput/index.js
@@ -8,9 +8,27 @@ exports.default = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
-var _reactQuill = _interopRequireDefault(require("react-quill"));
+var _tinymceReact = require("@tinymce/tinymce-react");
 
-require("react-quill/dist/quill.snow.css");
+var _tinymce = _interopRequireDefault(require("tinymce/tinymce"));
+
+require("tinymce/themes/silver");
+
+require("tinymce/icons/default");
+
+require("tinymce/skins/ui/oxide/skin.min.css");
+
+require("tinymce/plugins/link");
+
+require("tinymce/plugins/autolink");
+
+require("tinymce/plugins/lists");
+
+require("tinymce/plugins/paste");
+
+require("tinymce/plugins/quickbars");
+
+require("./index.css");
 
 var _ = require("../");
 
@@ -20,69 +38,61 @@ var _configSlice = require("../../LayoutEditor/configSlice");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+// TinyMCE so the global var exists
+// eslint-disable-next-line no-unused-vars
+// Theme
+// Toolbar icons
+// Editor styles
+// importing the plugin js.
+//Style overrides Override
 function RichTextInput(_ref) {
   var label = _ref.label,
       name = _ref.name,
       value = _ref.value,
-      formats = _ref.formats,
+      toolbar = _ref.toolbar,
       labelHidden = _ref.labelHidden,
       helpText = _ref.helpText,
       onChange = _ref.onChange;
   var cssClasses = (0, _reactRedux.useSelector)(_configSlice.selectCssClasses),
-      defaultFormats = ['bold', 'italic', 'list', 'bullet', 'link'];
+      defaultToolbar = ['undo redo bold italic | bullist numlist | superscript subscript | removeformat'];
   return /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, /*#__PURE__*/_react.default.createElement(_.FieldLabel, {
     htmlFor: name,
     visuallyHidden: labelHidden
-  }, label), /*#__PURE__*/_react.default.createElement(CustomToolbar, {
-    name: name
-  }), /*#__PURE__*/_react.default.createElement(_reactQuill.default, {
-    value: value || '',
-    onChange: onChange,
-    modules: {
-      toolbar: "#".concat(name, "Toolbar")
+  }, label), /*#__PURE__*/_react.default.createElement("div", {
+    id: "toolbar",
+    style: {
+      height: '2em'
+    }
+  }), /*#__PURE__*/_react.default.createElement("div", {
+    style: {
+      padding: '0.5rem',
+      border: '1px solid #757575'
+    }
+  }, /*#__PURE__*/_react.default.createElement(_tinymceReact.Editor, {
+    init: {
+      selector: 'textarea#default',
+      menubar: false,
+      inline: true,
+      skin: false,
+      browser_spellcheck: true,
+      contextmenu: false,
+      toolbar: toolbar || defaultToolbar,
+      fixed_toolbar_container: '#toolbar',
+      toolbar_persist: true,
+      plugins: 'quickbars link autolink lists paste',
+      link_context_toolbar: true,
+      link_quicklink: true,
+      link_title: false,
+      link_default_protocol: 'https',
+      quickbars_insert_toolbar: false,
+      quickbars_selection_toolbar: 'bold italic | quicklink',
+      valid_elements: 'p,ul,ol,li,strong,em,sup,sub,a[href]'
     },
-    formats: formats || defaultFormats,
-    theme: "snow"
-  }), helpText && /*#__PURE__*/_react.default.createElement("span", {
+    onEditorChange: onChange,
+    value: value || ''
+  })), helpText && /*#__PURE__*/_react.default.createElement("span", {
     className: cssClasses['help-text']
   }, ' ', helpText, ' '));
-}
-
-function CustomToolbar(_ref2) {
-  var name = _ref2.name;
-  return /*#__PURE__*/_react.default.createElement("div", {
-    id: "".concat(name, "Toolbar")
-  }, /*#__PURE__*/_react.default.createElement("span", {
-    className: "ql-formats"
-  }, /*#__PURE__*/_react.default.createElement("button", {
-    className: "ql-bold",
-    "aria-label": "Bold"
-  }), /*#__PURE__*/_react.default.createElement("button", {
-    className: "ql-italic",
-    "aria-label": "Italics"
-  })), /*#__PURE__*/_react.default.createElement("span", {
-    className: "ql-formats"
-  }, /*#__PURE__*/_react.default.createElement("button", {
-    className: "ql-list",
-    value: "ordered",
-    "aria-label": "Ordered List"
-  }), /*#__PURE__*/_react.default.createElement("button", {
-    className: "ql-list",
-    value: "bullet",
-    "aria-label": "Bulleted List"
-  })), /*#__PURE__*/_react.default.createElement("span", {
-    className: "ql-formats"
-  }, /*#__PURE__*/_react.default.createElement("button", {
-    className: "ql-link",
-    type: "button",
-    "aria-label": "Link"
-  })), /*#__PURE__*/_react.default.createElement("span", {
-    className: "ql-formats"
-  }, /*#__PURE__*/_react.default.createElement("button", {
-    className: "ql-clean",
-    type: "button",
-    "aria-label": "Clear Formatting"
-  })));
 }
 
 var _default = RichTextInput;
